e288cfef847190f47cd8fcb1454965c868204600
Ryan Northey <ryan@3ca.org.uk>
2012-05-13 06:15:49 +0100

    pep8/pyflakes

---
+++ bit/plone/project/browser/projects.py
@@ -0,0 +1,13 @@
+from Products.Five import BrowserView as FiveView
+
+from bit.plone.project.interfaces import IProject
+
+
+class ProjectView(FiveView):
+    def create_project_folders(self):
+        IProject(self.context).add_contacts_folder()
+        IProject(self.context).add_news_folder()
+        IProject(self.context).add_events_folder()
+        IProject(self.context).add_links_folder()
+        IProject(self.context).add_media_folder()
+        IProject(self.context).add_info_folder()

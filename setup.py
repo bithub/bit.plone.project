b6d0e6f79edb237e15a0ed4ad5feaa7b7508a888
Ryan Northey <ryan@3ca.org.uk>
2012-05-17 10:50:50 +0100

    "incrementing release version"

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.0.17'
+version = '0.0.18'
 
 
 setup(name='bit.plone.project',

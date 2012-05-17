45c86bc2597fe7008fbc078368c2f5ebd9a27262
Ryan Northey <ryan@3ca.org.uk>
2012-05-17 11:02:06 +0100

    vocabulary

---
+++ bit/plone/project/vocabulary/project_vocab.py
@@ -0,0 +1,29 @@
+import base64
+
+from zope.interface import implements
+from zope.component import getUtility, adapts
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+
+class ProjectStatusVocabulary(object):
+    """Vocabulary factory for ProjectStatus.
+    This confuses me. I need this to work with non-english tags which seem
+    to confuse zope vocabularies...
+    Need to find a more elegant way of dealing with the issue ;)
+    """
+    implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        statuses = dict(active='Active',
+                        ongoing='Ongoing',
+                        occasional='Occasional',
+                        complete='Complete',
+                        )
+        return SimpleVocabulary(
+            [SimpleTerm(value=k, token=base64.b64encode(k), title=v)
+             for k, v in statuses.items()])
+        
+
+ProjectStatusVocabularyFactory = ProjectStatusVocabulary()
+

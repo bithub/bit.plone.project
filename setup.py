f02f05027714715452881ede9deb48562b91cfc2
Ryan Northey <ryan@3ca.org.uk>
2012-05-23 00:39:16 +0100

    bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.0.24'
+version = '0.1.0'
 
 
 setup(name='bit.plone.project',
